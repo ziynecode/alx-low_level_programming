6ba11cc789bcb02ffd21e55871aadc78256dc877
ziynecode <ziyne.tech@gmail.com>
2023-02-20 11:05:23 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,12 @@ int main(void)
 {
   int tens;
   int ones;
-
-  for (tens = 0; tens <= 9; tens++)
+  for (tens = 0; tens <=9; tens++)
     {
-      for (ones = tens + 1; ones <= 9; ones++)
+      for (ones = tens +1; ones <= 9; ones++)
 	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+	  putchar(tens +'0');
+	  putchar(ones +'0');
 
 	  if (tens < 8)
 	    {
