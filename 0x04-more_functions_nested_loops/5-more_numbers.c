fbe360573b066b5d2b6c165c0f0fdcf444cabdea
ziynecode <ziyne.tech@gmail.com>
2023-02-24 15:16:03 -0800

    hard things no fit end me

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,20 @@
 #include "main.h"
-
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 void more_numbers(void)
 {
-	char n, c;
-	int i = 0;
+	int i;
+	char c;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		for (n = 0; n <= 14; n++)
+		for (c = 0; c <= 14; c++)
 		{
-			c = n;
-			if (n > 9)
-			{
-				_putchar('1');
-				c = n % 10;
-			}
-			_putchar('0' + c);
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
-		_putchar('0' + c);
-		i++;
+		_putchar('\n');
 	}
 }
